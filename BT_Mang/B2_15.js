0966409214639ecde0846540ccd4f271b06bba68
Nguyen Dung <79398833+nguyendug@users.noreply.github.com>
2023-01-16 00:46:15 +0700

    update_1

---
+++ BT_Mang/B2_15.js
@@ -0,0 +1,17 @@
+let n = prompt("Nhập n là số mã số cần kiểm tra: ");
+let arr = []
+for (let i = 0; i < n; i++) {
+    arr[i] = prompt(` Nhập mã số thứ ${i + 1}`);
+}
+console.log(arr)
+
+for (let i = 0; i < n; i++) {
+    const check = (arr) => {
+        let kq = /.00[2-5]L*\d{4}.*/
+        if (arr[i].match(kq) && arr[i].length == 8)
+            return `Đúng rồi`;
+        else
+            return `Sai rồi`;
+    }
+    console.log(check(arr))
+}
--- BT_Vong_Lap/B2_10.js
+++ BT_Vong_Lap/B2_10.js
@@ -1,7 +1,14 @@
-//Bài này đang làm rở
 let n = prompt("Nhập n là số mã sinh viên cần kiểm tra")
 for (let i = 0; i < n; i++) {
     let msv = prompt("Nhập mã sinh viên: ")
-    console.log("Mã sinh viên thứ: ", msv)
+    console.log(`Mã sinh viên thứ: ${i + 1}`, msv)
+    const check = (str) => {
+        let kq = /.B*\d{7}.*/
+        if (str.match(kq) && str.length == 8)
+            return `Mã sinh viên vừa nhập hợp lệ`;
+        else
+            return `Mã sinh viên vừa nhập không hợp lệ`;
+    }
+    console.log(check(msv))
 }
-console.log("hiihihihih")
+

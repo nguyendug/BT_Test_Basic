27249d19abcd452a516220d230ca28a6d7f947ba
Nguyen Dung <79398833+nguyendug@users.noreply.github.com>
2023-01-15 23:37:29 +0700

    Tester

---
+++ BT_Vong_Lap/B2_10.js
@@ -0,0 +1,7 @@
+//Bài này đang làm rở
+let n = prompt("Nhập n là số mã sinh viên cần kiểm tra")
+for (let i = 0; i < n; i++) {
+    let msv = prompt("Nhập mã sinh viên: ")
+    console.log("Mã sinh viên thứ: ", msv)
+}
+console.log("hiihihihih")
